9ffa771165005ca3ee3308deaacbac2099127496
Michele Scarlato <michele.scarlato@gmail.com>
2021-07-07 14:40:51 +0900

    set assetdata

--- importCSV/tourismCSV_to_JSON_single_Row.py
+++ importCSV/tourismCSV_to_JSON_single_Row.py
@@ -48,6 +48,7 @@ if __name__ == "__main__":
     aRow = 300 # max index: 651
     outputDict = rowExtraction(aRow)
     print(json.dumps(outputDict))
+    assetdata = json.dumps(outputDict)
     sendTransatcionToBigChainDB(assetdata)
     # save into JSON    # [alternatively you can perform the transaction here ]
     # with open("output"+str(aRow)+".json", "w") as jsonFile:
